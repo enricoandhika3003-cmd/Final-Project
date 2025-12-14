3707674a5d3c0938a12889106a873dfc53c16568
enricoandhika3003-cmd <enrico.andhika3003@gmail.com>
2025-12-14 18:47:11 +0700

    done 8

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ credit_button.addEventListener('click', function(){
 	var message_el = document.getElementById('message');
 	var account_balance = parseInt(current_balance_el.innerText) + parseInt(entered_amount_el);
 	if(!entered_amount_el){
-		message_el.innerText = "Please enter amount.";
+		message_el.innerText = "Please enter amount. 💵";
 		message_el.style.color = "#ff0000";
 	}
 	else{
@@ -16,7 +16,7 @@ credit_button.addEventListener('click', function(){
 			message_el.innerText = "";
 		}
 		else{
-			message_el.innerText = "Please enter amount greater than 0";
+			message_el.innerText = "Please enter amount greater than 0.";
 			message_el.style.color = "#ff0000";
 		}
 	}
@@ -30,11 +30,11 @@ debit_button.addEventListener('click', function(){
 	var account_balance = parseInt(current_balance_el.innerText) - parseInt(entered_amount_el);
 	
 	if(!entered_amount_el){
-		message_el.innerText = "Please enter amount.";
+		message_el.innerText = "Please enter amount. 💵";
 		message_el.style.color = "#ff0000";
 	}
 	else if(parseInt(entered_amount_el) > parseInt(current_balance_el.innerText)){
-		message_el.innerText = "You don't have balance.Please enter lesser amount.";
+		message_el.innerText = "You don't have enough money. 😢 Please enter smaller amount.";
 		message_el.style.color = "#ff0000";
 	}
 	else{
